542a7be626cc93241f9758a32a7af37b4ad1c775
dionisiy1986@gmail.com <dionisiy1986@gmail.com>
2020-01-22 11:58:49 +0200

    v 1.19.0

---
+++ astronomy/scripts/action_button.js
@@ -0,0 +1,210 @@
+function open_links(id) {
+  switch(id) {
+    // Mercury
+    case 'MR-3':
+                window.open("https://en.wikipedia.org/wiki/Mercury-Redstone_3","_blank");
+                break;
+    case 'MR-4':
+                window.open("https://en.wikipedia.org/wiki/Mercury-Redstone_4","_blank");
+                break;
+
+    case 'MA-6':
+                window.open("https://en.wikipedia.org/wiki/Mercury-Atlas_6","_blank");
+                break;
+
+    case 'MA-7':
+               window.open("https://en.wikipedia.org/wiki/Mercury-Atlas_7","_blank");
+               break;
+
+    case 'MA-8':
+               window.open("https://en.wikipedia.org/wiki/Mercury-Atlas_8","_blank");
+               break;
+
+    case 'MA-9':
+                window.open("https://en.wikipedia.org/wiki/Mercury-Atlas_9","_blank");
+                break;
+
+    case 'MR-2':
+               window.open("https://en.wikipedia.org/wiki/Mercury-Redstone_2","_blank");
+               break;
+
+    case 'MA-5':
+               window.open("https://en.wikipedia.org/wiki/Mercury-Atlas_5","_blank");
+               break;
+    // Gemini
+    case 'G34':
+               window.open("https://en.wikipedia.org/wiki/Gemini_3","_blank");
+               window.open("https://en.wikipedia.org/wiki/Gemini_4","_blank");
+               break;
+
+    case 'G5':
+              window.open("https://en.wikipedia.org/wiki/Gemini_5","_blank");
+              break;
+
+    case 'G7':
+              window.open("https://en.wikipedia.org/wiki/Gemini_7","_blank");
+              break;
+
+    case 'G6A':
+              window.open("https://en.wikipedia.org/wiki/Gemini_6A","_blank");
+              break;
+
+    case 'G8':
+              window.open("https://en.wikipedia.org/wiki/Gemini_8","_blank");
+              break;
+
+    case 'G9A':
+              window.open("https://en.wikipedia.org/wiki/Gemini_9A","_blank");
+              break;
+
+    case 'G10':
+              window.open("https://en.wikipedia.org/wiki/Gemini_10","_blank");
+              break;
+
+    case 'G11':
+              window.open("https://en.wikipedia.org/wiki/Gemini_11","_blank");
+              break;
+
+    case 'G12':
+              window.open("https://en.wikipedia.org/wiki/Gemini_12","_blank");
+              break;
+    // Apollo
+    case 'APL1':
+              window.open("https://en.wikipedia.org/wiki/Apollo_1","_blank");
+              break;
+
+    case 'APL456':
+               window.open("https://en.wikipedia.org/wiki/Apollo_4","_blank");
+               window.open("https://en.wikipedia.org/wiki/Apollo_5","_blank");
+               window.open("https://en.wikipedia.org/wiki/Apollo_6","_blank");
+               break;
+
+    case 'APL78':
+              window.open("https://en.wikipedia.org/wiki/Apollo_7","_blank");
+              window.open("https://en.wikipedia.org/wiki/Apollo_8","_blank");
+              break;
+
+    case 'APL910':
+              window.open("https://en.wikipedia.org/wiki/Apollo_9","_blank");
+              window.open("https://en.wikipedia.org/wiki/Apollo_10","_blank");
+              break;
+
+    case 'APL11':
+              window.open("https://en.wikipedia.org/wiki/Apollo_11","_blank");
+              break;
+
+    case 'APL1213':
+                   window.open("https://en.wikipedia.org/wiki/Apollo_12","_blank");
+                   window.open("https://en.wikipedia.org/wiki/Apollo_13","_blank");
+                   break;
+
+    case 'APL1415':
+                    window.open("https://en.wikipedia.org/wiki/Apollo_14","_blank");
+                    window.open("https://en.wikipedia.org/wiki/Apollo_15","_blank");
+                    break;
+
+    case 'APL1617':
+                  window.open("https://en.wikipedia.org/wiki/Apollo_16","_blank");
+                  window.open("https://en.wikipedia.org/wiki/Apollo_17","_blank");
+                  break;
+
+     // Space Shuttle
+
+    case 'STS1':
+                 window.open("https://en.wikipedia.org/wiki/STS-1","_blank");
+                 break;
+
+    case 'STS60':
+                window.open("https://en.wikipedia.org/wiki/STS-60","_blank");
+                break;
+
+    case 'STS31':
+                window.open("https://en.wikipedia.org/wiki/STS-31","_blank");
+                break;
+
+    case 'STS51F':
+                window.open("https://en.wikipedia.org/wiki/STS-51-F","_blank");
+                break;
+
+
+    case 'STS88':
+               window.open("https://en.wikipedia.org/wiki/STS-88","_blank");
+               break;
+
+    case 'STS51L':
+              window.open("https://en.wikipedia.org/wiki/STS-51-L","_blank");
+              break;
+
+
+    case 'STS107':
+              window.open("https://en.wikipedia.org/wiki/STS-107","_blank");
+              break;
+
+
+    case 'STS135':
+              window.open("https://en.wikipedia.org/wiki/STS-135","_blank");
+              break;
+
+    // Skylab
+
+    case 'SL1':
+              window.open("https://en.wikipedia.org/wiki/Skylab","_blank");
+              break;
+
+    case 'SL2':
+              window.open("https://en.wikipedia.org/wiki/Skylab_2","_blank");
+              break;
+
+    case 'SL3':
+              window.open("https://en.wikipedia.org/wiki/Skylab_3","_blank");
+              break;
+
+    case 'SL4':
+              window.open("https://en.wikipedia.org/wiki/Skylab_4","_blank");
+              break;
+
+      // ISS
+
+    case 'Building':
+                  window.open("https://en.wikipedia.org/wiki/International_Space_Station","_blank");
+                  break;
+
+    case 'ISS1-6':
+                window.open("https://en.wikipedia.org/wiki/Expedition_1","_blank");
+                window.open("https://en.wikipedia.org/wiki/Expedition_2","_blank");
+                window.open("https://en.wikipedia.org/wiki/Expedition_3","_blank");
+                window.open("https://en.wikipedia.org/wiki/Expedition_4","_blank");
+                window.open("https://en.wikipedia.org/wiki/Expedition_5","_blank");
+                window.open("https://en.wikipedia.org/wiki/Expedition_6","_blank");
+                break;
+
+    case 'ISS7-12':
+                window.open("https://en.wikipedia.org/wiki/Expedition_7","_blank");
+                window.open("https://en.wikipedia.org/wiki/Expedition_8","_blank");
+                window.open("https://en.wikipedia.org/wiki/Expedition_9","_blank");
+                window.open("https://en.wikipedia.org/wiki/Expedition_10","_blank");
+                window.open("https://en.wikipedia.org/wiki/Expedition_11","_blank");
+                window.open("https://en.wikipedia.org/wiki/Expedition_12","_blank");
+                break;
+
+    case 'ISS13-19':
+                window.open("https://en.wikipedia.org/wiki/Expedition_13","_blank");
+                window.open("https://en.wikipedia.org/wiki/Expedition_14","_blank");
+                window.open("https://en.wikipedia.org/wiki/Expedition_15","_blank");
+                window.open("https://en.wikipedia.org/wiki/Expedition_16","_blank");
+                window.open("https://en.wikipedia.org/wiki/Expedition_17","_blank");
+                window.open("https://en.wikipedia.org/wiki/Expedition_18","_blank");
+                window.open("https://en.wikipedia.org/wiki/Expedition_19","_blank");
+                break;
+
+    case 'ISS20-21':
+               window.open("https://en.wikipedia.org/wiki/Expedition_20","_blank");
+               window.open("https://en.wikipedia.org/wiki/Expedition_21","_blank");
+               break;
+
+    case 'Regular':
+              window.open("https://en.wikipedia.org/wiki/List_of_International_Space_Station_expeditions","_blank");
+              break;
+
+    }
+}
